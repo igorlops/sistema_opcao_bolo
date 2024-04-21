c318b0dc642fc5852b6a92bee2fdee8f3dbb0450
Claudio Igor Lopes Fernandes <t_cifernandes@sfiec.org.br>
2024-04-21 10:35:13 -0300

    correção da query para relatorio finaceiro

--- app/Http/Controllers/RelatorioFinanceiro.php
+++ app/Http/Controllers/RelatorioFinanceiro.php
@@ -29,11 +29,14 @@ class RelatorioFinanceiro extends Controller
                 ->format('d/m/Y')
             ]);
         }
-        dd($$request);
+        $data_inicial = \data_br_to_iso($request->data_inicial);
+        $data_final = \data_br_to_iso($request->data_final);
+        // dd($request);
         $users = User::all();
         $formaPagamentos = TipoPagamento::all();
         $tipoSaidas = TipoSaida::all();
         $produtos = Produto::all();
+
 
         $user_id = $request->usuario_select;
         $formaPagamento = $request->formaPagamento;
@@ -41,26 +44,28 @@ class RelatorioFinanceiro extends Controller
         $produto = $request->produto;
 
         $pagamentos = TipoPagamento::select('tipo_pagamentos.id', 'tipo_pagamentos.nome')
-    ->selectRaw('(SELECT SUM(entradas.valor)
-    FROM entradas
-    WHERE entradas.id_tipo_pagamento = tipo_pagamentos.id
-    '.($user_id || $user_id != '' ? "AND entradas.user_id = '$user_id'" : '').'
-    '.($formaPagamento || $formaPagamento != '' ? "AND entradas.id_tipo_pagamento = $formaPagamento" : '').'
-    '.($produto || $produto != '' ? "AND entradas.id_produto = $produto" : '').'
-    AND entradas.created_at BETWEEN "'.$request->data_inicial.' 00:00:00" AND "'.$request->data_final.' 23:59:59") as soma_valores')
-    ->whereBetween('tipo_pagamentos.created_at', ["$request->data_inicial 00:00:00", "$request->data_final 23:59:59"])
-    ->groupBy('tipo_pagamentos.id', 'tipo_pagamentos.nome')
-    ->get();
+                ->selectRaw('(SELECT SUM(entradas.valor)
+                    FROM entradas
+                    WHERE entradas.id_tipo_pagamento = tipo_pagamentos.id
+                    '.($user_id || $user_id != '' ? "AND entradas.user_id = '$user_id'" : '').'
+                    '.($formaPagamento || $formaPagamento != '' ? "AND entradas.id_tipo_pagamento = $formaPagamento" : '').'
+                    '.($produto || $produto != '' ? "AND entradas.id_produto = $produto" : '').'
+                    AND entradas.created_at
+                    BETWEEN "'.$data_inicial.' 00:00:00" AND "'.$data_final.' 23:59:59") as soma_valores')
+                ->whereBetween('tipo_pagamentos.created_at', [$data_inicial ." 00:00:00", $data_final ." 23:59:59"])
+                ->groupBy('tipo_pagamentos.id', 'tipo_pagamentos.nome')
+                ->get();
 
 
         $saida = TipoSaida::select('tipo_saidas.descricao','tipo_saidas.id')
-                ->selectRaw('(SELECT SUM("saidas.valor")
-                FROM saidas
-                WHERE saidas.id_descricao = tipo_saidas.id
-                '.$user_id || $user_id != '' ? "AND saidas.user_id = $user_id " : ''.'
-                '.$tipoSaida || $tipoSaida != '' ? "AND saidas.id_descricao = $tipoSaida " : ''.'
-                AND saidas.created_at BETWEEN "'.$request->data_inicial.' 00:00:00" AND "'.$request->data_final.' 23:59:59") as soma_saidas)')
-                ->whereBetween('created_at',[$request->data_inicial,$request->data_final])
+                ->selectRaw('(SELECT SUM(saidas.valor)
+                    FROM saidas
+                    WHERE saidas.id_descricao = tipo_saidas.id
+                    '.($user_id || $user_id != '' ? "AND saidas.user_id = $user_id " : '').'
+                    '.($tipoSaida || $tipoSaida != '' ? "AND saidas.id_descricao = $tipoSaida " : '').'
+                    AND saidas.created_at BETWEEN "'.$data_inicial.' 00:00:00"
+                                            AND "'.$data_final.' 23:59:59") as soma_saidas')
+                ->whereBetween('created_at',[$data_inicial,$data_final])
                 ->groupBy('tipo_saidas.descricao','tipo_saidas.id')
                 ->get();
 
@@ -68,11 +73,12 @@ class RelatorioFinanceiro extends Controller
                 ->selectRaw('(SELECT COUNT("*")
                 FROM entradas
                 WHERE entradas.id_produto = produtos.id
-                '.$user_id || $user_id != '' ? "AND entradas.user_id = '$user_id" : '' .'
-                '.$formaPagamento || $formaPagamento != '' ? "AND entradas.id_tipo_pagamento = $formaPagamento " : ''.'
-                '.$produto || $produto != '' ? "AND entradas.id_produto = $produto " : ''.'
-                AND entradas.created_at BETWEEN "'.$request->data_inicial.' 00:00:00" AND "'.$request->data_final.' 23:59:59") as contador_produtos)')
-                ->whereBetween('created_at',[$request->data_inicial,$request->data_final])
+                '.($user_id || $user_id != '' ? "AND entradas.user_id = '$user_id" : '' ).'
+                '.($formaPagamento || $formaPagamento != '' ? "AND entradas.id_tipo_pagamento = $formaPagamento " : '').'
+                '.($produto || $produto != '' ? "AND entradas.id_produto = $produto " : '').'
+                AND entradas.created_at BETWEEN "'.$data_inicial.' 00:00:00"
+                                        AND "'.$data_final.' 23:59:59") as contador_produtos')
+                ->whereBetween('created_at',[$data_inicial,$data_final])
                 ->groupBy('produtos.nome','produtos.id')
                 ->get();
 
